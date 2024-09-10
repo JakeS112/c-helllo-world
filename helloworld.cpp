5ea0e2606d662fdd90257d563a8676ecc46aa6ac
JakeS112 <jake.sweat@mcvts.org>
2024-09-10 10:34:27 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+ int whole = 5;
+ float dec = 5.25;
+ char letter = 'z';
+ bool bo = true;
+ std::string line = "Im done";
+
+int main()
+{
+    std::cout << whole << std::endl;
+    std::cout << dec << std::endl;
+    std::cout << letter << std::endl;
+    std::cout << bo << std::endl;
+    std::cout << line << std::endl;
+}
